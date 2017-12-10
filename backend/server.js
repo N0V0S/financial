4f49d3ba2c7e6e5a48801522df14c8467ee79d00
N0v0s <flt@b4sh.de>
2017-12-10 20:40:51 +0100

    Allows CORS

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,13 @@ mongoose.connect('mongodb://localhost/finance');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
 var routes = require('./api/routes/financialRoutes');
 routes(app); 
 
